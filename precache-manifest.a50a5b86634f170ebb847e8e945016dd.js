e1af20de475c0f5a5e6ed9c23f07d1482acbec1e
Saicharan67 <mahankalisaicharan@gmail.com>
2020-11-01 13:48:54 +0530

    Updates

---
+++ precache-manifest.a50a5b86634f170ebb847e8e945016dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b79e8abcdeb290f570e74b518287d9a",
+    "url": "/Unsplash-Clone/index.html"
+  },
+  {
+    "revision": "972aa15b266aa39b2ffc",
+    "url": "/Unsplash-Clone/static/css/main.71ad4ae4.chunk.css"
+  },
+  {
+    "revision": "b840323518eafc76560e",
+    "url": "/Unsplash-Clone/static/js/2.dda7d1cd.chunk.js"
+  },
+  {
+    "revision": "972aa15b266aa39b2ffc",
+    "url": "/Unsplash-Clone/static/js/main.a133a7ac.chunk.js"
+  },
+  {
+    "revision": "c5adba14eb02d6a20eea",
+    "url": "/Unsplash-Clone/static/js/runtime~main.3658472a.js"
+  }
+]);
